be9f1a971e959c921826d4077d7acb1d58b63c8c
changhooni <reviewsize@gmail.com>
2025-07-14 13:23:02 +0900

    Add files via upload

---
+++ power_calculator.py
@@ -0,0 +1,5 @@
+import math
+
+input_number = int(input('제곱을 구할 값을 입력하세요 : '))
+power_number = int(input('몇 제곱 값을 입력하세요 : '))
+print(input_number, '의', power_number, '제곱 = ', math.pow(input_number, power_number))
