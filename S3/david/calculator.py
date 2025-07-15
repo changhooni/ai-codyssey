17f9c3248ebbf5c2407e453c9b7119b0fa4b75b3
changhooni <reviewsize@gmail.com>
2025-07-15 15:54:08 +0900

    Add files via upload

--- S3/david/calculator.py
+++ S3/david/calculator.py
@@ -42,5 +42,40 @@ def main():
         result = 'Invalid operator.'
     print(f"Result : {result}")
 
+text_num = input('연산식을 입력하세요 : ').split()
+
+def main1():
+    result = 0
+    i = 0
+    for i in text_num:
+        a = int(float(text_num[0])) #text_num 대한 split 변수값에 첫번째 요소 지정
+        b = int(float(text_num[2])) #text_num 대한 split 변수값에 세번째 요소 지정
+        operator = text_num[1] #text_num 대한 split 변수값에 두번째 요소 지정(연산자 확인)
+    
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = subtract(a, b)
+    elif operator == "*":
+        result = multiply(a, b)
+    elif operator == "/":
+        if b == 0:
+            result = 'Error : Division by zero.'
+        else:
+            result = divide(a, b)
+    else:
+        result = 'Invalid operator.'
+    print(f"Result : {result}")
+
+    # eval() 함수를 사용하여 한줄 연산하기
+    #try:
+    #    result = eval(operator)
+    #    print(f"Result : {result}")
+    #except (SyntaxError, NameError):
+    #    print("Invalid operator.")
+    #except Exception as e:
+    #    print("Error :", e)
+
 if __name__ == '__main__':
     main()
+    main1()
--- S3/david/calculator_plus.py
+++ S3/david/calculator_plus.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def add(a, b):
     return a + b
  
-def subtract(x, b):
+def subtract(a, b):
     return a - b
  
 def multiply(a, b):
@@ -14,17 +14,38 @@ def multiply(a, b):
 def divide(a, b):
     return a / b
 
-operator = input('계산할 숫자를 입력하세요 : ')
+text_num = input('연산식을 입력하세요 : ').split()
 
 def main():
+    result = 0
+    i = 0
+    for i in text_num:
+        a = int(float(text_num[0]))
+        b = int(float(text_num[2]))
+        operator = text_num[1]
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = subtract(a, b)
+    elif operator == "*":
+        result = multiply(a, b)
+    elif operator == "/":
+        if b == 0:
+            result = 'Error : Division by zero.'
+        else:
+            result = divide(a, b)
+    else:
+        result = 'Invalid operator.'
+    print(f"Result : {result}")
+
     # eval() 함수를 사용하여 한줄 연산하기
-    try:
-        result = eval(operator)
-        print(f"Result : {result}")
-    except (SyntaxError, NameError):
-        print("Invalid operator.")
-    except Exception as e:
-        print("Error :", e)
+    #try:
+    #    result = eval(operator)
+    #    print(f"Result : {result}")
+    #except (SyntaxError, NameError):
+    #    print("Invalid operator.")
+    #except Exception as e:
+    #    print("Error :", e)
 
 
 if __name__ == '__main__':
