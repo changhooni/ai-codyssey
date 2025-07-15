6579162a553c7528bfe1b7dede0fe68e3a96943b
changhooni <reviewsize@gmail.com>
2025-07-15 20:07:06 +0900

    Add files via upload

--- S5/sort_calculator.py
+++ S5/sort_calculator.py
@@ -3,13 +3,24 @@ from flask import Flask
 app = Flask(__name__)
 
 def selection_sort(data):
-    numbers = [float(num) for num in data.split()] # 입력한 값을 split 공백 처리를 하고 반복문으로 num에 값을 넣어줌 값을 다시 float로 변환함
+    #numbers = [float(num) for num in data.split()] # 입력한 값을 split 공백 처리를 하고 반복문으로 num에 값을 넣어줌 값을 다시 float로 변환함
+    items = data.split()
+    numbers = []
+
+    for item in items:
+        try:
+            numbers.append(float(item))
+        except ValueError:
+            print("Invalid operator.")
+
     n = len(numbers) # 입력된 데이터에 갯수
     for i in range(n):
         min_index = i
         for j in range(i+1, n):
             if numbers[j] < numbers[min_index]:
                 min_index = j
+            else:
+                numbers = print("Invalid input.")
         numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
     return numbers
 
